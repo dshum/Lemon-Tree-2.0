ad4a266bef3f4c1b058f00e028f4b52acdfd00bd
dshum <denis-shumeev@yandex.ru>
2011-09-22 22:07:48 +0400

    Modify all.

--- www/LT/tree.php
+++ www/LT/tree.php
@@ -77,7 +77,6 @@
 			$model->
 			set('selfUrl', PATH_ADMIN.'index.php?module='.$controllerName)->
 			set('baseUrl', PATH_ADMIN.'index.php')->
-			set('controllerName', $controllerName)->
 			set('loggedUser', $loggedUser);
 		}
 
